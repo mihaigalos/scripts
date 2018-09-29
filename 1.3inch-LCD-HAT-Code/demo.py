26d5a2e8f676b0941bf479d3832fec59b8d18a42
Mihai Galos <mihaigalos@gmail.com>
2018-09-29 14:27:27 +0200

    Add statistics including df from Fred

--- 1.3inch-LCD-HAT-Code/demo.py
+++ 1.3inch-LCD-HAT-Code/demo.py
@@ -14,6 +14,21 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
+
+def getFredDiskUsage(drive):
+	cmd =  "ssh osmc@192.168.0.101 'df -h' | grep " + drive
+        command_result = subprocess.check_output(cmd, shell = True )
+	elements=command_result.split()
+
+	total_size=elements[1]
+	used_size=elements[2]
+	used_percentage=elements[4]
+	path=elements[5][elements[5].find("/", 1)+1:]
+	path=path[-1:]
+
+	return path +": "+str(used_size)+"/"+str(total_size)+" "+str(used_percentage)
+
+
 # Load default font.
 font = ImageFont.load_default()
 
@@ -51,14 +66,11 @@ try:
 	while True:
 		with canvas(device) as draw:
 
-			#draw.rectangle(device.bounding_box, outline="white", fill="black")
+			#draw.rectangle(device.bounding_box, outline="black", fill="black")
 			#draw.text((30, 40), "Hello World", fill="white")
 			# Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-			cmd = "hostname -I | cut -d\' \' -f1"
-			IP = subprocess.check_output(cmd, shell = True )
-			#cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-			#cmd = "top -bn2 | grep '%Cpu' | tail -1 | grep -Poh  '(...|...) us'"
-			#cmd =  "top -bn2 | grep '%Cpu' | tail -1 | grep -Poh  '(...|...) us' | head -c 3"
+			#cmd = "hostname -I | cut -d\' \' -f1"
+			#IP = subprocess.check_output(cmd, shell = True )
 			cmd =  "top -bn2 | grep '%Cpu' | tail -1 | grep -Poh '([0-9.]*) us' | head -c 4"
 
 			CPU = subprocess.check_output(cmd, shell = True )
@@ -68,13 +80,29 @@ try:
 			cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
 			Disk = subprocess.check_output(cmd, shell = True )
 
-			# Write two lines of text.
+			cmd = "python /home/mihai/scripts/mvg/getMVGInfo_Petuelring_U3.py"
+			MvgInfo=subprocess.check_output(cmd, shell = True )
 
-			draw.text((x, top),       "IP: " + str(IP),  font=font, fill=255)
-			draw.text((x, top+8),     str(CPU), font=font, fill=255)
-			draw.text((x, top+16),    str(MemUsage),  font=font, fill=255)
-			draw.text((x, top+25),    str(Disk),  font=font, fill=255)
-		time.sleep(3)
-except:
-	print("except")
+			# Write two lines of text.
+			draw.text((x, top),    str(MvgInfo),  font=font, fill=255)
+
+			cmd = "python /home/mihai/scripts/mvg/getMVGInfo_Milbertshofen_U2.py"
+			MvgInfo_U2=subprocess.check_output(cmd, shell = True )
+
+			cmd = "python /home/mihai/scripts/google/getTimeToWork.py"
+			time_to_work=subprocess.check_output(cmd, shell = True )
+
+			draw.text((x+6*8, top),    str(MvgInfo_U2),  font=font, fill=255)
+			draw.text((x, top+8),    str("Mins to work: ")+str(time_to_work)+".",  font=font, fill=255)
+
+			#draw.text((x, top),       "IP: " + str(IP),  font=font, fill=255)
+			draw.text((x, top+16),     str(CPU), font=font, fill=255)
+			draw.text((x, top+25),    str(MemUsage),  font=font, fill=255)
+			draw.text((x, top+33),    str(Disk),  font=font, fill=255)
+			#draw.text((x, top+33),    str(MvgInfo),  font=font, fill=255)
+			draw.text((x, top+41),    getFredDiskUsage("SeagateA"),  font=font, fill=255)
+			draw.text((x, top+49),    getFredDiskUsage("SeagateB"),  font=font, fill=255)
+			time.sleep(30)
+except Exception as e:
+	print(e)
 GPIO.cleanup()
