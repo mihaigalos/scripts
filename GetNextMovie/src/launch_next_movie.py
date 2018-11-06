a4dec1dbf7235ba7944312874dc4a6f87ee3c391
Mihai Galos <mihaigalos@gmail.com>
2018-11-06 17:55:26 +0100

    Add launch script

---
+++ GetNextMovie/src/launch_next_movie.py
@@ -0,0 +1,17 @@
+from subprocess import call
+from getnextmovie import MovieSeasonAndEpisodeExtractor
+
+
+def get_next_movie():
+    msae = MovieSeasonAndEpisodeExtractor()
+    msae.readLog()
+    (season, episode, folder) = msae.parseLastMovie()
+    next_suggested_movie = msae.getNextMovie()
+    return next_suggested_movie
+
+
+next_movie = get_next_movie()
+
+args = ["kodi-send", "--action='PlayMedia(" + next_movie + ")'"]
+print(args)
+# call(args)
