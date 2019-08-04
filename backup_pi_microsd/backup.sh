138e04b97d10d3e6f8c93951b022a572facb398b
Mihai Galos <mihaigalos@gmail.com>
2019-08-04 20:20:12 +0200

    Removing superfluous newlines

--- backup_pi_microsd/backup.sh
+++ backup_pi_microsd/backup.sh
@@ -10,13 +10,13 @@ sudo chmod a+x pishrink.sh
 date_suffix=$(date +%y%m%d) 
 output_filename=pi_$date_suffix.img
 
-echo "\nDumping microSD contents.." 
+echo "Dumping microSD contents.."
 sudo dd if=/dev/sdc | pv | dd of=pi_backup.img
 
-echo "\nShrinking and producing final image.."
+echo "Shrinking and producing final image.."
 sudo ./pishrink.sh -s pi_backup.img $output_filename
 
-echo "\nRemoving initial image dump.."
+echo "Removing initial image dump.."
 rm pi_backup.img
 
-echo "\nDone. Written file: $output_filename"
+echo "Done. Written file: $output_filename"
