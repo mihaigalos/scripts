7a618810fe1d0226b9b18e6fed8d33aa7133489e
Mihai Galos <mihaigalos@gmail.com>
2019-11-29 00:03:29 +0100

    Get file, add to transmission

--- DownloadFromTracker/get_movie.sh
+++ DownloadFromTracker/get_movie.sh
@@ -6,9 +6,12 @@ declare movie="${@:4:99}"
 
 movie=${movie// /+} # substitute spaces for plusses
 
-echo curl --cookie-jar /tmp/tracker_cookies.txt --form username="$username" --form password="$password" "$tracker"
-suggestions=$(curl -b /tmp/tracker_cookies.txt https://filelist.ro/browse.php?search=$movie&cat=0&searchin=1&sort=2)
+curl --silent --cookie-jar /tmp/tracker_cookies.txt --form username="$username" --form password="$password" "$tracker"/takelogin.php
+suggestions=$(curl --silent -b /tmp/tracker_cookies.txt "$tracker"/browse.php?search=$movie&cat=0&searchin=1&sort=1)
 
 torrent=$(echo $suggestions | sed -n 's/.*\(download.php?id=[0-9]*\).*/\1/p')
 echo $torrent
+curl --silent -b /tmp/tracker_cookies.txt "$tracker"/$torrent > /tmp/new_torrent.torrent
+
+transmission-remote localhost:9091 -a /tmp/new_torrent.torrent 
 
