9af54bb6e5eef244806fead7c77778871dbb8388
Mihai Galos <mihaigalos@gmail.com>
2019-11-29 16:55:14 +0100

    DRY the install script

--- DownloadFromTracker/install.sh
+++ DownloadFromTracker/install.sh
@@ -1,2 +1,3 @@
-sudo ln -s `realpath invoke_get_movie.sh` /usr/local/bin/invoke_get_movie.sh
-echo alias get=\"invoke_get_movie.sh\" >> ~/.zshrc
+target="invoke_get_movie.sh"
+sudo ln -s `realpath "$target"` /usr/local/bin/"$target"
+echo alias get=\"$target\" >> ~/.zshrc
