09cc4c27cf06a72de37ba8658d0e87a8e1e722da
Mihai Galos <mihaigalos@gmail.com>
2019-11-29 11:31:17 +0100

    Redirect temp

--- DownloadFromTracker/get_movie.sh
+++ DownloadFromTracker/get_movie.sh
@@ -4,10 +4,12 @@ declare password=$2
 declare tracker=$3
 declare movie="${@:4:99}"
 
+temp_folder="/mnt/Vera_SeagateC/incomplete"
+
 movie=${movie// /+} # substitute spaces for plusses
 
-curl --silent --cookie-jar /tmp/tracker_cookies.txt --form username="$username" --form password="$password" "$tracker"/takelogin.php
-suggestions=$(curl --silent -b /tmp/tracker_cookies.txt "$tracker"/browse.php?search=$movie&cat=0&searchin=1&sort=1)
+curl --silent --cookie-jar "$temp_folder/tracker_cookies.txt" --form username="$username" --form password="$password" "$tracker"/takelogin.php
+suggestions=$(curl --silent -b "$temp_folder/tracker_cookies.txt" "$tracker"/browse.php?search=$movie&cat=0&searchin=1&sort=1)
 
 choice=`echo "$suggestions" \
 | sed -e "s/href='details.php?/\n/g" \
@@ -17,6 +19,6 @@ choice=`echo "$suggestions" \
 | xargs dialog --stdout --clear --backtitle "Backtitle here" --title "Title here" --menu "Choose one of the following options:" 15 60 4`
 
 torrent="$tracker/download.php?id=$choice"
-curl --silent -b /tmp/tracker_cookies.txt "$torrent" > /tmp/new_torrent.torrent
+curl --silent -b "$temp_folder/tracker_cookies.txt" "$torrent" > "$temp_folder"/new_torrent.torrent
 
-transmission-remote localhost:9091 -a /tmp/new_torrent.torrent
+transmission-remote localhost:9091 -a "$temp_folder"/new_torrent.torrent
