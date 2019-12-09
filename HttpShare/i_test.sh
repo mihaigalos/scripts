194f90d4bdfeb9e5099f86903187982431022e65
Mihai Galos <mihaigalos@gmail.com>
2019-12-09 23:08:00 +0100

    Adhere to AAA

--- HttpShare/i_test.sh
+++ HttpShare/i_test.sh
@@ -1,17 +1,30 @@
 #! /bin/bash
 
-temp_folder=$(mktemp -d)
-touch "$temp_folder"/foo
-touch "$temp_folder"/bar
+arrange() {
+    temp_folder=$(mktemp -d)
+    touch "$temp_folder"/foo
+    touch "$temp_folder"/bar
+}
 
-./share.sh $temp_folder 7001
-sleep 2s
-result=$(curl --silent http://0.0.0.0:7001)
+act() {
+    ./share.sh $temp_folder 7001
+    sleep 2s
+    result=$(curl --silent http://0.0.0.0:7001)
+}
 
-if [[ $result == *"foo"* ]] && [[ $result == *"bar"* ]]; then
-  echo "Everything OK."
-else
-  echo "Could not share folder on specified port."
-fi
+assert() {
+    if [[ $result == *"foo"* ]] && [[ $result == *"bar"* ]]; then
+      echo "Everything OK."
+    else
+      echo "Could not share folder on specified port."
+    fi
+}
 
-docker ps | grep python_http_server | awk '{print $1}' | xargs docker kill > /dev/null
+teardown() {
+    docker ps | grep python_http_server | awk '{print $1}' | xargs docker kill > /dev/null
+}
+
+arrange
+act
+assert
+teardown
