c8934cdf7062fbf7d50a43c1469e33f59acf5dc9
Mihai Galos <mihaigalos@gmail.com>
2019-12-09 23:36:00 +0100

    Use unused port in arrange

--- HttpShare/i_test.sh
+++ HttpShare/i_test.sh
@@ -1,15 +1,27 @@
 #! /bin/bash
 
+use_unused_port() {
+    port=0
+    while
+      port=$(shuf -n 1 -i 49152-65535)
+      netstat -atun | grep -q "$port"
+    do
+      continue
+    done
+}
+
 arrange() {
     temp_folder=$(mktemp -d)
+
     touch "$temp_folder"/foo
     touch "$temp_folder"/bar
+    use_unused_port
 }
 
 act() {
-    ./share.sh $temp_folder 7001
+    ./share.sh $temp_folder $port
     sleep 2s
-    result=$(curl --silent http://0.0.0.0:7001)
+    result=$(curl --silent "http://0.0.0.0:$port")
 }
 
 assert() {
