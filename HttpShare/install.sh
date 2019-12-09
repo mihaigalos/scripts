be20188e325f177e87b1346352b24525fcd33e2c
Mihai Galos <mihaigalos@gmail.com>
2019-12-09 22:33:37 +0100

    Tidy up install

--- HttpShare/install.sh
+++ HttpShare/install.sh
@@ -1,14 +1,21 @@
-cat <<EOT >Dockerfile
-FROM python:latest
-EXPOSE 8000
-CMD cd /share
-CMD python -m http.server 8000
+build_docker() {
+    cat <<EOT >Dockerfile
+    FROM python:latest
+    EXPOSE 8000
+    CMD cd /share
+    CMD python -m http.server 8000
 EOT
 
-docker build -t python_http_server .
+    docker build -t python_http_server .
 
-rm Dockerfile
+    rm Dockerfile
+}
 
-target="share.sh"
-sudo ln -s `realpath "$target"` /usr/local/bin/"$target"
-echo alias share=\"$target\" >> ~/.zshrc
+make_available_everywhere() {
+    target="share.sh"
+    sudo ln -s `realpath "$target"` /usr/local/bin/"$target"
+    echo alias share=\"$target\" >> ~/.zshrc
+}
+
+build_docker
+make_available_everywhere
