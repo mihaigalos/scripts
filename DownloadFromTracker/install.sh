4c70386eca9c56393211626bfacd447e69c592af
Mihai Galos <mihaigalos@gmail.com>
2020-02-08 15:24:54 +0100

    Adapt install

--- DownloadFromTracker/install.sh
+++ DownloadFromTracker/install.sh
@@ -1,3 +1,3 @@
 target="invoke_get_movie.sh"
 sudo ln -s `realpath "src/$target"` /usr/local/bin/"$target"
-echo alias get=\"`realpath "src/$target"`\" >> ~/.zshrc
+echo alias get=\""$target"\" >> ~/.zshrc
