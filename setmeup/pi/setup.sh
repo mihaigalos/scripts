e052efecde216e52cfcc42a3a46dfbbf2969b2f2
Mihai Galos <mihaigalos@gmail.com>
2020-03-09 23:36:34 +0100

    Only run file in the pi folder if on Pi

--- setmeup/pi/setup.sh
+++ setmeup/pi/setup.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-for file in ./setup_*
-do
-    bash -c $file
-done
+rasperry_pi_model_file="/proc/device-tree/model"
+
+function run_files_with_setup_prefix() {
+    for file in ./setup_*
+    do
+        echo bash -c $file
+    done
+}
+
+if [ -f "$rasperry_pi_model_file" ]; then
+    if grep -q Raspberry "$rasperry_pi_model_file"
+    then
+        run_files_with_setup_prefix
+    fi
+fi
 
