b89d32d06b76241ac7e2e972d8fbd9a86c2552e7
Mihai Galos <mihaigalos@gmail.com>
2020-03-09 18:13:42 +0000

    Fix fetching of molokai

--- setmeup/setup_vim.sh
+++ setmeup/setup_vim.sh
@@ -3,7 +3,7 @@
 function setup_vim() {
     mkdir -p ~/.vim/colors
     pushd ~/.vim/colors
-    https://raw.githubusercontent.com/mihaigalos/molokai/master/colors/molokai.vim
+    wget https://raw.githubusercontent.com/mihaigalos/molokai/master/colors/molokai.vim
     popd
     
     pushd ~
