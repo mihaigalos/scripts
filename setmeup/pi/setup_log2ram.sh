e137881510d6c7ff9ed263a9f67a1d91101bc7ab
Mihai Galos <mihaigalos@gmail.com>
2020-03-09 23:17:04 +0100

    Add setup for pi

---
+++ setmeup/pi/setup_log2ram.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function setup_log2ram() {
+    curl -Lo log2ram.tar.gz https://github.com/azlux/log2ram/archive/master.tar.gz
+    tar xf log2ram.tar.gz
+    cd log2ram-master
+    chmod +x install.sh && sudo ./install.sh
+    cd ..
+    rm -r log2ram-master
+}
+
+setup_log2ram
---
+++ setmeup/pi/setup_pivpn.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+function setup_pivpn() {
+    curl -L https://install.pivpn.io | bash
+}
+
+setup_pivpn
