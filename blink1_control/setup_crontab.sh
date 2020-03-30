9b7e730c1a87dda238a5012b82f9e5e9913c2a5b
Mihai Galos <mihaigalos@gmail.com>
2020-03-30 21:58:56 +0200

    Abstract away current folder

--- blink1_control/setup_crontab.sh
+++ blink1_control/setup_crontab.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-(crontab -l 2>/dev/null; echo "*/5 * * * * python ~/git/utils/blink1_control/flash_blink1_on_ip_change.py >/dev/null 2>&1") | crontab -
-(crontab -l 2>/dev/null; echo "*/5 * * * * python ~/git/utils/blink1_control/no_vpn.py >/dev/null 2>&1") | crontab -
-(crontab -l 2>/dev/null; echo "*/1 * * * * python ~/git/utils/blink1_control/downloading.py >/dev/null 2>&1") | crontab -
+folder=$(pwd)
+
+(crontab -l 2>/dev/null; echo "*/5 * * * * python ${folder}/flash_blink1_on_ip_change.py >/dev/null 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "*/5 * * * * python ${folder}/no_vpn.py >/dev/null 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "*/1 * * * * python ${folder}/downloading.py >/dev/null 2>&1") | crontab -
