004f9fdc3fcd6cc6e7361c21fadfb5044e73dbaa
Mihai Galos <mihaigalos@gmail.com>
2020-03-30 21:43:38 +0200

    Encode color in flashes

--- blink1_control/downloading.py
+++ blink1_control/downloading.py
@@ -6,6 +6,22 @@ import subprocess
 home = expanduser("~")
 kBlinkOneTool = home+"/git/utils/blink1_control/blink1-tool"
 
+class SystemProvider():
+    def get_speed(self):
+        bashCommand = "sudo iftop  -B -t -s 1 2>/dev/null | grep Total\ receive\ rate: | awk '{print $4}'"
+        output, error = subprocess.Popen(
+                bashCommand, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+            ).communicate()
+        return self.to_bytes(output)
+
+    def to_bytes(self, input):
+        units = {"B": 1, "KB": 2**10, "MB": 2**20, "GB": 2**30, "TB": 2**40}
+        input = input.upper()
+        if not re.match(r' ', input):
+            input = re.sub(r'([KMGT]?B)', r' \1', input)
+        number, unit = [string.strip() for string in input.split()]
+        return int(float(number)*units[unit])
+
 
 class TranmissionProvider():
     def get(self):
@@ -27,10 +43,23 @@ class TranmissionProvider():
             return True
         return False
 
-def blink_14_times_a_4_seconds():
+def flash_led(times, delay, color):
     command = "sudo "+kBlinkOneTool + \
-        " --led=2 --green --blink 20 --delay 1500 --millis 1500 > /dev/null"
+        " --led=2 --"+color+" --blink "+str(times)+" --delay "+str(delay)+" --millis 1500 > /dev/null"
     os.system(command)
 
+def get_color_for_speed(speed):
+    speed=int(speed)
+    if speed<500000:
+        return "red"
+    elif speed < 1500000:
+        return "yellow"
+    else:
+        return "green"
+
+speed_bytes_per_second=SystemProvider().get_speed()
+color=get_color_for_speed(speed_bytes_per_second)
+
 if TranmissionProvider().is_active_download():
-    blink_14_times_a_4_seconds()
+    flash_led(times=20, delay=1500, color=color)
+
