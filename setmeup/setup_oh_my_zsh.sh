3618fb064133516d520c7d9c63b8b383d069d8ef
Mihai Galos <mihaigalos@gmail.com>
2020-09-18 21:09:10 +0200

    Remove git PR push

--- setmeup/setup_oh_my_zsh.sh
+++ setmeup/setup_oh_my_zsh.sh
@@ -18,28 +18,6 @@ export LC_ALL="en_US.UTF-8"
 function cd {
     builtin cd "\$@" && l
 }
-
-unalias gp &>/dev/null || true
-function gp() {
-    declare branch origin param
-    declare -a gp_opts=()
-    while param="${1:-}"; [[ -n "$param" ]]; do
-        [[ "$param" == -* ]] || break
-        shift
-        gp_opts+=("$param")
-    done
-    origin="${1:-$(git config --get branch.master.remote)}" || return 1
-    branch="${2:-$(git rev-parse --abbrev-ref HEAD)}" || return 1
-    if ! git rev-parse --abbrev-ref --symbolic-full-name '@{u}' &>/dev/null; then
-        gp_opts+=(-u)
-    fi
-    gp_opts+=("$origin" "$branch")
-    echo git push "${gp_opts[@]}"
-    git push "${gp_opts[@]}"
-    last_commit_message=$(git reflog -1 | sed 's/^.*: //')
-    hub pull-request -F- <<<"WIP $last_commit_message"
-}
-
 EOF
 }
 
