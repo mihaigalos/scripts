5612743931655b50baf1fb0e5eb9563029903dd8
Mihai Galos <mihaigalos@gmail.com>
2020-10-17 13:56:35 +0200

    Set theme before calling oh-my-zsh.sh

--- setmeup/setup_oh_my_zsh.sh
+++ setmeup/setup_oh_my_zsh.sh
@@ -6,11 +6,10 @@ function setup_oh_my_zsh() {
     sudo apt -y install zsh
 
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    sed -i -e "s/ZSH_THEME.*//" ~/.zshrc
+    sed -i -e 's/ZSH_THEME.*/ZSH_THEME="af-magic-time"/' ~/.zshrc
     wget https://gist.githubusercontent.com/mihaigalos/bde132c03ba2ae6a5f4d5c0cfedbcd61/raw/3fe5d6461005981bd529e124f728a88e5949063c/af-magic-time.zsh-theme -O ~/.oh-my-zsh/themes/af-magic-time.zsh-theme
 
     cat << EOF >> ~/.zshrc
-ZSH_THEME="af-magic-time"
 HISTFILE=~/.zsh_history
 HISTSIZE=9999999
 SAVEHIST=$HISTSIZE
