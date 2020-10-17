762088866f1098d5feddb275e0664724fb77ba67
Mihai Galos <mihaigalos@gmail.com>
2020-10-17 12:47:13 +0200

    Strip preexisting zsh theme before installing af-magic-time

--- setmeup/setup_oh_my_zsh.sh
+++ setmeup/setup_oh_my_zsh.sh
@@ -6,7 +6,7 @@ function setup_oh_my_zsh() {
     sudo apt -y install zsh
 
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+    sed -i -e "s/ZSH_THEME.*//" ~/.zshrc
     wget https://gist.githubusercontent.com/mihaigalos/bde132c03ba2ae6a5f4d5c0cfedbcd61/raw/3fe5d6461005981bd529e124f728a88e5949063c/af-magic-time.zsh-theme ~/.oh-my-zsh/themes
 
     cat << EOF >> ~/.zshrc
