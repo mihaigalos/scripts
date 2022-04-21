02737e3318f0bab77ca4680ef0885b12f5620f5a
Mihai Galos <mihaigalos@gmail.com>
2022-04-21 22:28:37 +0200

    GitHub name: Test logic

--- github_name/runme.sh
+++ github_name/runme.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-set -x
 user=$1
 endpoint=https://github.com
 
-curl ${endpoint}/${user} | grep -- '<title>' | sed -e "s|.*<.*>\(.*\)</.*>|\1|" -e "s/[()]//g" | cut -d' ' -f1-3 | awk '{print  $1 " # " $2 " " $3}'
+curl -s ${endpoint}/${user} | grep -- '<title>' | sed -e "s|.*<.*>\(.*\)</.*>|\1|" -e "s/[()]//g" | cut -d' ' -f1-3 | awk '{print  $1 " # " $2 " " $3}'
